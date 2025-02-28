bcd6a2ba6f4aba31d29a4e382fe0b9e378ea9227
Dinuka Pramod <dinukah98@gmail.com>
2025-02-28 14:58:08 +0530

    explicit wait added

--- app/src/test/java/org/example/waits/ExcplicitWait.java
+++ app/src/test/java/org/example/waits/ExcplicitWait.java
@@ -5,10 +5,15 @@ import java.time.Duration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 // used in 5%
+//Unlike Implicit Wait, which applies a fixed delay for all elements, Explicit Wait is customized for each element and waits only when necessary.
 
 public class ExcplicitWait {
 
@@ -24,7 +29,29 @@ public class ExcplicitWait {
 
     @Test
     public void basicTest() {
-      
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("openOnlyFirst")));
+
+        // an element will return only if its available depends on the expected cond.
+        // in this case its just the visibility
+
+        //still not found after 10 secs, it will throw an error
+
+        // there are more
+        /*
+         * elementToBeClickable
+         * presenceOfElementLocated
+         * textToBePresentInElementLocated
+         * alertIsPresent
+         */
+
+         element.click();
+         System.out.println("button text: "+ element.getText());
+
+         WebElement secBtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("openAll")));
+         System.out.println("sec btn text:" + secBtn.getText());
     }
 
     @AfterEach
